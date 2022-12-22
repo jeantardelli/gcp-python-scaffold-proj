460ddafca68d79d792ea5a54e74603086d86ee03
jeantardelli <jeantardelli@gmail.com>
2022-12-22 08:40:57 +0000

    Adding first files from GCP

---
+++ hello.py
@@ -0,0 +1,6 @@
+def add(x, y):
+    return x + y
+
+
+result = add(1, 2)
+print("This is the sum of 1 and 2: %s" % result)
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import add
+
+
+def test_add():
+    assert add(1, 2) == 3
